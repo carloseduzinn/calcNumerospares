247319a44faf717566333d70cba08e2ee1036f02
carlos.efspires <carlos.efspires@SBC0505243W10-1>
2023-06-15 11:18:06 -0300

    Adicionar arquivos de projeto.

---
+++ calcNumerospares/Controller/calcularnumeropar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calcNumerospares.Controller
+{
+    internal class calcularnumeropar
+    {
+        public string Calcular(int valor)
+        {
+
+            string resul = "";
+
+            for (int  i = 0; i <= valor; i += 2 )
+            {
+                resul +=  + i + ",";
+            }
+            return resul;
+        }
+    }
+}
---
+++ calcNumerospares/View/numerospares.cs
@@ -0,0 +1,30 @@
+﻿using calcNumerospares.Controller;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calcNumerospares.View
+{
+    public partial class numerospares : Form
+    {
+        
+        public numerospares()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            calcularnumeropar Calcularnumeropar = new calcularnumeropar();
+            int valor = Convert.ToInt32(textBox1.Text);
+            label1.Text = Calcularnumeropar.Calcular(valor);
+
+        }
+    }
+}
